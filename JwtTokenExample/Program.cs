c890ee73eaab7471626a7a85ab7aa94ca5da95d3
Михаил Иванов <mikhail.ivanov@travelline.ru>
2023-10-12 12:42:49 +0300

    Добавлен проект JwtTokenExample

---
+++ JwtTokenExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+using RSA rsa = RSA.Create();
+rsa.ImportFromPem(File.ReadAllText("Secrets/rsa_private_key_for_jwt.pem"));
+
+SecurityKey securityKey = new RsaSecurityKey(rsa);
+SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256);
+
+SecurityTokenDescriptor descriptor = new()
+{
+    Issuer = "IssuerName",
+    Audience = "AudienceName",
+    Expires = DateTime.UtcNow.AddMinutes(5),
+    SigningCredentials = signingCredentials,
+    Subject = new ClaimsIdentity(new Claim[]
+    {
+        new(ClaimTypes.NameIdentifier, "exampleUser123")
+    })
+};
+
+JwtSecurityTokenHandler tokenHandler = new();
+SecurityToken token = tokenHandler.CreateToken(descriptor);
+string jwt = tokenHandler.WriteToken(token);
+
+Console.WriteLine($"JWT: {jwt}");
+
+try
+{
+    tokenHandler.ValidateToken(
+        jwt,
+        new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = "IssuerName",
+            ValidAudience = "AudienceName",
+            IssuerSigningKey = securityKey
+        },
+        out SecurityToken _);
+
+    Console.WriteLine("JWT токен прошел верификацию!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("JWT токен НЕ прошел верификацию!");
+}
---
+++ JwtTokenExample/Program2.cs
@@ -0,0 +1,32 @@
+﻿// using System.IdentityModel.Tokens.Jwt;
+// using System.Security.Cryptography;
+// using Microsoft.IdentityModel.Tokens;
+//
+// string jwt = "<jwt_token_code>"; // Укажите тут токен, который нужно верифицировать
+//
+// using RSA rsa = RSA.Create();
+// rsa.ImportFromPem(File.ReadAllText("Secrets/rsa_public_key_for_jwt.pem"));
+//
+// JwtSecurityTokenHandler tokenHandler = new();
+//
+// try
+// {
+//     tokenHandler.ValidateToken(
+//         jwt,
+//         new TokenValidationParameters
+//         {
+//             ValidateIssuerSigningKey = true,
+//             ValidateIssuer = true,
+//             ValidateAudience = true,
+//             ValidIssuer = "IssuerName",
+//             ValidAudience = "AudienceName",
+//             IssuerSigningKey = new RsaSecurityKey(rsa)
+//         },
+//         out SecurityToken _);
+//
+//     Console.WriteLine("JWT токен прошел верификацию!");
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine("JWT токен НЕ прошел верификацию!");
+// }
